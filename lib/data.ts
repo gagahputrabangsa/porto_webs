94576995949f925acb4e7050b969fa85a88ecdd0
gagahputrabangsa <gagaputra626@gmail.com>
2024-06-08 19:16:32 +0700

    updated data

--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,9 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
+import dapenImg from "@/public/dana_pensiun_1.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+import legalImg from "@/public/legal_viewer.png";
 import ohcleanerImg from "@/public/ohcleaner.png";
 
 export const links = [
@@ -39,7 +41,7 @@ export const experiencesData = [
     title: "Majoring In IT - Trisakti University",
     location: "Grogol, West Jakarta",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "Studying Information Technology at State Polytechnic of Malang with a high GPA of 3,78.",
     icon: React.createElement(LuGraduationCap),
     date: "2021 - Present",
   },
@@ -48,40 +50,40 @@ export const experiencesData = [
     location: "Grogol, West Jakarta",
     description:
       "Create a website that is used to record pension fund transactions.",
-    icon: React.createElement(CgWorkAlt),
+    icon: React.createElement(FaReact),
     date: "Jul - Oct 2023",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Data/Software Engineer",
+    location: "Central Jakarta, DKI Jakarta",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+      "I participated in the MSIB Batch 6 internship program at the Kementerian Keuangan Republik Indonesia as a Data/Software Engineer. During my internship, I was responsible for developing a legal viewer web application. The project involved using NextJS, and TailwindCSS for front-end development, Prisma for database management, and Typescript for data extraction and modelling.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Feb - Jun 2024",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "Oh Cleaner!",
+    title: "Legal Viewer JDIH Kemenkeu",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["Figma"],
-    imageUrl: ohcleanerImg,
-  },
+    "A web application designed for recording and managing pension fund transactions, specifically used by the employees of Universitas Trisakti.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Node.js","prisma"],
+    imageUrl: legalImg,
+    },
+      {
+        title: "Oh Cleaner!",
+        description:
+          "An user interfaces for mobile applications that can be used to order cleaning services",
+        tags: ["Figma","Photoshop"],
+        imageUrl: ohcleanerImg,
+      },
   {
-    title: "rmtDev",
+    title: "Pension Fund Web",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "A web application designed for recording and managing pension fund transactions, specifically used by the employees of Universitas Trisakti.",
+    tags: ["Python", "Django", "JavaScript", "Html", "Css"],
+    imageUrl: dapenImg,
   },
 ] as const;
 
